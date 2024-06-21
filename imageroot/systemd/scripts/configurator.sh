b7cd9c05868017bfada95a7af084199a76f04f40
kemboi22 <kemboielvis22@gmail.com>
2024-06-21 23:01:05 +0300

    feat: variables renaming

--- imageroot/systemd/scripts/configurator.sh
+++ imageroot/systemd/scripts/configurator.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ls -1 apps > sites/apps.txt
-bench set-config -g db_host $$DB_HOST
-bench set-config -gp db_port $$DB_PORT
-bench set-config -g redis_cache "redis://$$REDIS_CACHE"
-bench set-config -g redis_queue "redis://$$REDIS_QUEUE"
-bench set-config -g redis_socketio "redis://$$REDIS_QUEUE"
-bench set-config -gp socketio_port $$SOCKETIO_PORT
+bench set-config -g db_host "$DB_HOST"
+bench set-config -gp db_port "$DB_PORT"
+bench set-config -g redis_cache "redis://$REDIS_CACHE"
+bench set-config -g redis_queue "redis://$REDIS_QUEUE"
+bench set-config -g redis_socketio "redis://$REDIS_QUEUE"
+bench set-config -gp socketio_port "$SOCKETIO_PORT"
