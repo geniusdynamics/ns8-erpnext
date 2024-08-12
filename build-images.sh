d9e2a03634997aa8d49ac024180a5f82f3dc17a5
Martin Bhuong <martin@genius.ke>
2024-08-12 13:24:22 +0200

    update docker image org

--- build-images.sh
+++ build-images.sh
@@ -45,7 +45,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@node:routeadm" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=docker.io/mariadb:10.11.5 docker.io/kemboi22/custom-erpnext:${app_version} docker.io/redis:6.2-alpine" \
+    --label="org.nethserver.images=docker.io/mariadb:10.11.5 docker.io/geniusdynamics/custom-erpnext:${app_version} docker.io/redis:6.2-alpine" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
