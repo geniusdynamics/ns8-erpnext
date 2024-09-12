24ee838aaf76e39823c630a2cddfd529f746eecc
Martin Bhuong <martin@genius.ke>
2024-09-12 07:32:19 +0200

    Upgate docker image version, with v 15.35.0

--- build-images.sh
+++ build-images.sh
@@ -14,7 +14,7 @@ images=()
 repobase="${REPOBASE:-ghcr.io/geniusdynamics}"
 # Configure the image name
 reponame="erpnext"
-app_version="v15.34.0-ns.4"
+app_version="v15.34.0-ns.5"
 # Create a new empty container image
 container=$(buildah from scratch)
 
