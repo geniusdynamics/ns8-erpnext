5304e4746984788d32b8ba67b7fb7312446add89
kemboi22 <kemboielvis22@gmail.com>
2025-02-14 13:08:08 +0300

    added a restore from backup module

--- ui/src/views/Settings.vue
+++ ui/src/views/Settings.vue
@@ -61,28 +61,29 @@
                 $t("settings.enabled")
               }}</template>
             </cv-toggle>
-            <div>Selected Modules: {{erpSelectedModules}} </div>
+            <div>Selected Modules: {{ erpSelectedModules }}</div>
             <cv-multi-select
-                :label="'ERP Next Modules to be installed'"
-                :options="erpNextModules"
-                :title="'ERP Next Modules to be installed'"
-                v-model="erpSelectedModules"
+              :label="'ERP Next Modules to be installed'"
+              :options="erpNextModules"
+              :title="'ERP Next Modules to be installed'"
+              v-model="erpSelectedModules"
             >
             </cv-multi-select>
-              <!-- advanced options -->
+            <!-- advanced options -->
             <cv-accordion ref="accordion" class="maxwidth mg-bottom">
               <cv-accordion-item :open="toggleAccordion[0]">
                 <template slot="title">{{ $t("settings.advanced") }}</template>
                 <template slot="content">
-
                   <div v-for="module in erpNextModules" :key="module.value">
                     <cv-toggle
-                        :label="module.value"
-                        :value="module.value"
-                        v-model="erpSelectedModules"
-                        :disabled="loading.getConfiguration || loading.configureModule"
-                        class="mg-bottom">
-
+                      :label="module.value"
+                      :value="module.value"
+                      v-model="erpSelectedModules"
+                      :disabled="
+                        loading.getConfiguration || loading.configureModule
+                      "
+                      class="mg-bottom"
+                    >
                     </cv-toggle>
                   </div>
                 </template>
@@ -105,6 +106,13 @@
               :disabled="loading.getConfiguration || loading.configureModule"
               >{{ $t("settings.save") }}</NsButton
             >
+            <NSButton
+              @click.prevent="restoreBackup"
+              kind="secondary"
+              :icon="Save20"
+            >
+              Restore From Backup
+            </NSButton>
           </cv-form>
         </cv-tile>
       </cv-column>
@@ -144,51 +152,215 @@ export default {
       host: "",
       isLetsEncryptEnabled: false,
       isHttpToHttpsEnabled: true,
-      erpNextModules:[
-        { "label": "ERPNext", "value": "erpnext", "name": "erpnext", "disabled": false },
-        { "label": "HRMS", "value": "hrms", "name": "hrms", "disabled": false },
-        { "label": "Nl Attendance Timesheet", "value": "nl_attendance_timesheet", "name": "nl-attendance-timesheet", "disabled": false },
-        { "label": "Nl Piece Rate Pay", "value": "nl_piece_rate_pay", "name": "nl-piece-rate-pay", "disabled": false },
-        { "label": "Employee Self Service", "value": "employee_self_service", "name": "employee_self_service", "disabled": false },
-        { "label": "Expenses", "value": "erpnext_expenses", "name": "erpnext_expenses", "disabled": false },
-        { "label": "payments", "value": "payments", "name": "payments", "disabled": false },
-        { "label": "Paystack", "value": "frappe_paystack", "name": "frappe_paystack", "disabled": false },
-        { "label": "Mpesa Payments", "value": "frappe_mpsa_payments", "name": "frappe-mpesa-payments", "disabled": false },
-        { "label": "Mpesa B2C", "value": "navari_mpesa_b2c", "name": "navari-mpesa-b2c", "disabled": false },
-        { "label": "KE Etims Compliance", "value": "kenya_compliance", "name": "kenya-compliance", "disabled": false },
-        { "label": "KE etims Compliance", "value": "kenya_etims_compliance", "name": "kenya_etims_compliance", "disabled": false },
-        { "label": "Csf Ke", "value": "csf_ke", "name": "csf_ke", "disabled": false },
-        { "label": "Whatsapp", "value": "frappe_whatsapp", "name": "frappe_whatsapp", "disabled": false },
-        { "label": "PibiDav", "value": "pibidav", "name": "pibidav", "disabled": false },
-        { "label": "PibiCard", "value": "pibicard", "name": "pibicard", "disabled": false },
-        { "label": "Pibicut", "value": "pibicut", "name": "pibicut", "disabled": false },
-        { "label": "PibiCal", "value": "pibical", "name": "pibical", "disabled": false },
-        { "label": "Jobcard Planning", "value": "jobcard_planning", "name": "jobcard_planning", "disabled": false },
-        { "label": "Print Designer", "value": "print_designer", "name": "print_designer", "disabled": false },
-        { "label": "Beam Barcode", "value": "beam", "name": "beam", "disabled": false },
-        { "label": "QR CODE", "value": "frappe_qrcode", "name": "frappe_qrcode", "disabled": false },
-        { "label": "PDF on submit", "value": "pdf_on_submit", "name": "pdf_on_submit", "disabled": false },
-        { "label": "Whitelabel", "value": "whitelabel", "name": "whitelabel", "disabled": false },
-        { "label": "Cloud Storage", "value": "cloud_storage", "name": "cloud_storage", "disabled": false },
-        { "label": "Check Run", "value": "check_run", "name": "check_run", "disabled": false },
-        { "label": "Inventory tools", "value": "inventory_tools", "name": "inventory_tools", "disabled": false },
-        { "label": "Lending", "value": "lending", "name": "lending", "disabled": false },
-        { "label": "HealthCare", "value": "healthcare", "name": "healthcare", "disabled": false },
-        { "label": "GetPOS", "value": "nbpos", "name": "nbpos", "disabled": false },
-        { "label": "POS Awesome", "value": "posawesome", "name": "posawesome", "disabled": false },
-        { "label": "Webshop", "value": "webshop", "name": "webshop", "disabled": false },
-        { "label": "Education", "value": "education", "name": "education", "disabled": false },
-        { "label": "LMS (SA)", "value": "lms", "name": "lms", "disabled": false },
-        { "label": "Wiki (SA)", "value": "wiki", "name": "wiki", "disabled": false },
-        { "label": "Helpdesk (SA)", "value": "helpdesk", "name": "helpdesk", "disabled": false },
-        { "label": "Insights (SA)", "value": "insights", "name": "insights", "disabled": false },
-        { "label": "Builder (SA)", "value": "builder", "name": "builder", "disabled": false },
-        { "label": "CRM (SA)", "value": "crm", "name": "crm", "disabled": false },
-        { "label": "Raven (SA)", "value": "raven", "name": "raven", "disabled": false },
-        { "label": "Gameplan (SA)", "value": "gameplan", "name": "gameplan", "disabled": false },
-        { "label": "Drive (SA)", "value": "drive", "name": "drive", "disabled": false }, 
-      ]
-      ,
+      erpNextModules: [
+        {
+          label: "ERPNext",
+          value: "erpnext",
+          name: "erpnext",
+          disabled: false,
+        },
+        { label: "HRMS", value: "hrms", name: "hrms", disabled: false },
+        {
+          label: "Nl Attendance Timesheet",
+          value: "nl_attendance_timesheet",
+          name: "nl-attendance-timesheet",
+          disabled: false,
+        },
+        {
+          label: "Nl Piece Rate Pay",
+          value: "nl_piece_rate_pay",
+          name: "nl-piece-rate-pay",
+          disabled: false,
+        },
+        {
+          label: "Employee Self Service",
+          value: "employee_self_service",
+          name: "employee_self_service",
+          disabled: false,
+        },
+        {
+          label: "Expenses",
+          value: "erpnext_expenses",
+          name: "erpnext_expenses",
+          disabled: false,
+        },
+        {
+          label: "payments",
+          value: "payments",
+          name: "payments",
+          disabled: false,
+        },
+        {
+          label: "Paystack",
+          value: "frappe_paystack",
+          name: "frappe_paystack",
+          disabled: false,
+        },
+        {
+          label: "Mpesa Payments",
+          value: "frappe_mpsa_payments",
+          name: "frappe-mpesa-payments",
+          disabled: false,
+        },
+        {
+          label: "Mpesa B2C",
+          value: "navari_mpesa_b2c",
+          name: "navari-mpesa-b2c",
+          disabled: false,
+        },
+        {
+          label: "KE Etims Compliance",
+          value: "kenya_compliance",
+          name: "kenya-compliance",
+          disabled: false,
+        },
+        {
+          label: "KE etims Compliance",
+          value: "kenya_etims_compliance",
+          name: "kenya_etims_compliance",
+          disabled: false,
+        },
+        { label: "Csf Ke", value: "csf_ke", name: "csf_ke", disabled: false },
+        {
+          label: "Whatsapp",
+          value: "frappe_whatsapp",
+          name: "frappe_whatsapp",
+          disabled: false,
+        },
+        {
+          label: "PibiDav",
+          value: "pibidav",
+          name: "pibidav",
+          disabled: false,
+        },
+        {
+          label: "PibiCard",
+          value: "pibicard",
+          name: "pibicard",
+          disabled: false,
+        },
+        {
+          label: "Pibicut",
+          value: "pibicut",
+          name: "pibicut",
+          disabled: false,
+        },
+        {
+          label: "PibiCal",
+          value: "pibical",
+          name: "pibical",
+          disabled: false,
+        },
+        {
+          label: "Jobcard Planning",
+          value: "jobcard_planning",
+          name: "jobcard_planning",
+          disabled: false,
+        },
+        {
+          label: "Print Designer",
+          value: "print_designer",
+          name: "print_designer",
+          disabled: false,
+        },
+        { label: "Beam Barcode", value: "beam", name: "beam", disabled: false },
+        {
+          label: "QR CODE",
+          value: "frappe_qrcode",
+          name: "frappe_qrcode",
+          disabled: false,
+        },
+        {
+          label: "PDF on submit",
+          value: "pdf_on_submit",
+          name: "pdf_on_submit",
+          disabled: false,
+        },
+        {
+          label: "Whitelabel",
+          value: "whitelabel",
+          name: "whitelabel",
+          disabled: false,
+        },
+        {
+          label: "Cloud Storage",
+          value: "cloud_storage",
+          name: "cloud_storage",
+          disabled: false,
+        },
+        {
+          label: "Check Run",
+          value: "check_run",
+          name: "check_run",
+          disabled: false,
+        },
+        {
+          label: "Inventory tools",
+          value: "inventory_tools",
+          name: "inventory_tools",
+          disabled: false,
+        },
+        {
+          label: "Lending",
+          value: "lending",
+          name: "lending",
+          disabled: false,
+        },
+        {
+          label: "HealthCare",
+          value: "healthcare",
+          name: "healthcare",
+          disabled: false,
+        },
+        { label: "GetPOS", value: "nbpos", name: "nbpos", disabled: false },
+        {
+          label: "POS Awesome",
+          value: "posawesome",
+          name: "posawesome",
+          disabled: false,
+        },
+        {
+          label: "Webshop",
+          value: "webshop",
+          name: "webshop",
+          disabled: false,
+        },
+        {
+          label: "Education",
+          value: "education",
+          name: "education",
+          disabled: false,
+        },
+        { label: "LMS (SA)", value: "lms", name: "lms", disabled: false },
+        { label: "Wiki (SA)", value: "wiki", name: "wiki", disabled: false },
+        {
+          label: "Helpdesk (SA)",
+          value: "helpdesk",
+          name: "helpdesk",
+          disabled: false,
+        },
+        {
+          label: "Insights (SA)",
+          value: "insights",
+          name: "insights",
+          disabled: false,
+        },
+        {
+          label: "Builder (SA)",
+          value: "builder",
+          name: "builder",
+          disabled: false,
+        },
+        { label: "CRM (SA)", value: "crm", name: "crm", disabled: false },
+        { label: "Raven (SA)", value: "raven", name: "raven", disabled: false },
+        {
+          label: "Gameplan (SA)",
+          value: "gameplan",
+          name: "gameplan",
+          disabled: false,
+        },
+        { label: "Drive (SA)", value: "drive", name: "drive", disabled: false },
+      ],
       erpSelectedModules: [],
       loading: {
         getConfiguration: false,
@@ -229,13 +401,13 @@ export default {
       // register to task error
       this.core.$root.$once(
         `${taskAction}-aborted-${eventId}`,
-        this.getConfigurationAborted
+        this.getConfigurationAborted,
       );
 
       // register to task completion
       this.core.$root.$once(
         `${taskAction}-completed-${eventId}`,
-        this.getConfigurationCompleted
+        this.getConfigurationCompleted,
       );
 
       const res = await to(
@@ -246,7 +418,7 @@ export default {
             isNotificationHidden: true,
             eventId,
           },
-        })
+        }),
       );
       const err = res[0];
 
@@ -301,34 +473,25 @@ export default {
         }
       }
     },
-    async configureModule() {
-      this.error.test_imap = false;
-      this.error.test_smtp = false;
-      const isValidationOk = this.validateConfigureModule();
-      if (!isValidationOk) {
-        return;
-      }
-
-      this.loading.configureModule = true;
-      const taskAction = "configure-module";
+    async restoreBackup() {
+      const taskAction = "restore-backup";
       const eventId = this.getUuid();
-
       // register to task error
       this.core.$root.$once(
         `${taskAction}-aborted-${eventId}`,
-        this.configureModuleAborted
+        this.configureModuleAborted,
       );
 
       // register to task validation
       this.core.$root.$once(
         `${taskAction}-validation-failed-${eventId}`,
-        this.configureModuleValidationFailed
+        this.configureModuleValidationFailed,
       );
 
       // register to task completion
       this.core.$root.$once(
         `${taskAction}-completed-${eventId}`,
-        this.configureModuleCompleted
+        this.configureModuleCompleted,
       );
       const res = await to(
         this.createModuleTaskForApp(this.instanceName, {
@@ -346,7 +509,63 @@ export default {
             description: this.$t("settings.configuring"),
             eventId,
           },
-        })
+        }),
+      );
+      const err = res[0];
+
+      if (err) {
+        console.error(`error creating task ${taskAction}`, err);
+        this.error.configureModule = this.getErrorMessage(err);
+        this.loading.configureModule = false;
+        return;
+      }
+    },
+    async configureModule() {
+      this.error.test_imap = false;
+      this.error.test_smtp = false;
+      const isValidationOk = this.validateConfigureModule();
+      if (!isValidationOk) {
+        return;
+      }
+
+      this.loading.configureModule = true;
+      const taskAction = "configure-module";
+      const eventId = this.getUuid();
+
+      // register to task error
+      this.core.$root.$once(
+        `${taskAction}-aborted-${eventId}`,
+        this.configureModuleAborted,
+      );
+
+      // register to task validation
+      this.core.$root.$once(
+        `${taskAction}-validation-failed-${eventId}`,
+        this.configureModuleValidationFailed,
+      );
+
+      // register to task completion
+      this.core.$root.$once(
+        `${taskAction}-completed-${eventId}`,
+        this.configureModuleCompleted,
+      );
+      const res = await to(
+        this.createModuleTaskForApp(this.instanceName, {
+          action: taskAction,
+          data: {
+            host: this.host,
+            lets_encrypt: this.isLetsEncryptEnabled,
+            http2https: this.isHttpToHttpsEnabled,
+            erpSelectedModules: this.erpSelectedModules,
+          },
+          extra: {
+            title: this.$t("settings.instance_configuration", {
+              instance: this.instanceName,
+            }),
+            description: this.$t("settings.configuring"),
+            eventId,
+          },
+        }),
       );
       const err = res[0];
 
