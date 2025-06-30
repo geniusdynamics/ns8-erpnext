80bbd5138ecee7b026673288b047070946bdb0d4
Kemboi Elvis <kemboielvis22@gmail.com>
2025-06-30 16:28:35 +0300

    fix: fixed the version

--- build-images.sh
+++ build-images.sh
@@ -15,7 +15,7 @@ repobase="${REPOBASE:-ghcr.io/geniusdynamics}"
 # Configure the image name
 reponame="erpnext"
 
-app_version="v15.66.1"
+app_version="15.66.1"
 
 # Create a new empty container image
 container=$(buildah from scratch)
