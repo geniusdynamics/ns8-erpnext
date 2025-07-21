3d82e29db49ab8c3b8cd84e32898513aa7109f45
Kemboi Elvis <kemboielvis22@gmail.com>
2025-07-21 17:51:24 +0300

    deps: updated to v15.70.2

--- build-images.sh
+++ build-images.sh
@@ -15,7 +15,7 @@ repobase="${REPOBASE:-ghcr.io/geniusdynamics}"
 # Configure the image name
 reponame="erpnext"
 
-app_version="15.69.2"
+app_version="15.70.2"
 
 # Create a new empty container image
 container=$(buildah from scratch)
