b4c1f30bf49b0169b6a6be4e21455b288ffc637d
Martin Bhuong <martin@genius.ke>
2025-12-12 06:05:05 +0300

    Update app version to 15.91.2

--- build-images.sh
+++ build-images.sh
@@ -15,7 +15,7 @@ repobase="${REPOBASE:-ghcr.io/geniusdynamics}"
 # Configure the image name
 reponame="erpnext"
 
-app_version="15.89.2"
+app_version="15.91.2"
 
 # Create a new empty container image
 container=$(buildah from scratch)
